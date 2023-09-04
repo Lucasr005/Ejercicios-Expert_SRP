042db78d096497ae0c99a1bd5e618c9b4d5fd8b8
Lucasr005 <lucas.rodrigueza@correo.ucu.edu.uy>
2023-09-03 21:23:38 -0300

    Cambios ej 1

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -2,27 +2,30 @@
 
 namespace Ucu.Poo.Expert
 {
-    public class Book
+public class Book
+{
+    public string Title { get; }
+    public string Author { get; }
+    public string Code { get; }
+
+    public Book(string title, string author, string code)
     {
-
-        public string Title { get ; }
-        public string Author { get ; }
-        public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
-
-        public Book(String title, String author, String code)
-        {
-            this.Title = title;
-            this.Author = author;
-            this.Code = code;
-        }
-
-        public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
-
+        this.Title = title;
+        this.Author = author;
+        this.Code = code;
     }
 }
+
+public class LibraryBook
+{
+    public string LibrarySector { get; set; }
+    public string LibraryShelve { get; set; }
+
+    public static void ShelveBook(string sector, string shelve)
+    {
+        this.LibrarySector = sector;
+        this.LibraryShelve = shelve;
+    }
+}
+}
+/*No cumple con el principio SRP por que tiene 2 responsabilidades, por lo tanto lo ideal seria dividirlo en 2 clases: Book y ShelveBook */
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,8 @@ namespace Ucu.Poo.Expert
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            book1.LibraryBook.ShelveBook("A","7");
+            book2.LibraryBook.ShelveBook("B","3");
 
         }
     }
